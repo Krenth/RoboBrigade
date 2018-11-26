b5f30a12c839adb276660f9fb2ab466627f160c3
emarchand3 <emarchand3@gatech.edu>
2018-11-26 18:30:08 -0500

    Fixed Bugs

--- Project6563.m
+++ Project6563.m
@@ -1,10 +1,10 @@
-%Initializing the agents to random positions with barrier certificates 
+%Initializing the agents to random positions with barrier certificates
 %and data plotting.  This script shows how to initialize robots to a
 %particular point
-%Paul Glotfelter 
+%Paul Glotfelter
 %3/24/2016
 
-N = 5;
+N = 3;
 r = Robotarium('NumberOfRobots', N, 'ShowFigure', true);
 
 % Initialize x so that we don't run into problems later.  This isn't always
@@ -15,10 +15,10 @@ r.step();
 % Create a barrier certificate so that the robots don't collide
 si_barrier_certificate = create_si_barrier_certificate('SafetyRadius', 0.22);
 si_to_uni_dynamics = create_si_to_uni_mapping2();
-        
+
 %Get randomized initial conditions in the robotarium arena
-%initial_conditions = generate_initial_conditions(N, 'Width', 1, 'Height',1, 'Spacing', 0.2);
-initial_conditions = [-1.2,-1.2,-1.2,-1.2,-1.2;.9,.45,0,-.45,-.9;0,0,0,0,0];
+initial_conditions = generate_initial_conditions(N, 'Width', 1, 'Height',1, 'Spacing', 0.2);
+
 
 % We'll make the rotation error huge so that the initialization checker
 % doesn't care about it
@@ -26,42 +26,36 @@ args = {'PositionError', 0.1, 'RotationError', 50};
 init_checker = create_is_initialized(args{:});
 controller = create_si_position_controller();
 
-while(~init_checker(x, initial_conditions))
-
-    x = r.get_poses();
-    dxi = controller(x(1:2, :), initial_conditions(1:2, :));
-    dxi = si_barrier_certificate(dxi, x(1:2, :));      
-    dxu = si_to_uni_dynamics(dxi, x);
-
-    r.set_velocities(1:N, dxu);
-    r.step();   
-end
+% while(~init_checker(x, initial_conditions))
+%     
+%     x = r.get_poses();
+%     dxi = controller(x(1:2, :), initial_conditions(1:2, :));
+%     dxi = si_barrier_certificate(dxi, x(1:2, :));
+%     dxu = si_to_uni_dynamics(dxi, x);
+%     
+%     r.set_velocities(1:N, dxu);
+%     r.step();
+% end
 
 %% Create the desired Laplacian
-iterations = 4000;
+iterations = 6500;
 l = .53;
 p = 1.5;
 q = .9183681179;
 %Graph laplacian
 
 L = zeros(N, N);
-A = [0,1,1,1,1;...
-     1,0,1,1,1;...
-     0,0,0,0,0;...
-     1,1,1,0,1;...
-     1,1,1,1,0];
-delta = [4,0,0,0,0;
-         0,4,0,0,0;
-         0,0,0,0,0;
-         0,0,0,4,0
-         0,0,0,0,4];
+A = [0,1,1;...
+    1,0,1;...
+    1,1,0];
+delta = [2,0,0;
+    0,2,0;
+    0,0,2];
 L = delta-A;
-weights = [0,l,l*2,q,p;
-           l,0,l,p/2,q;
-           0,0,0,0,0;
-           q,p/2,l,0,l;
-           p,q,l*2,l,0];
-          
+weights = [0,0.3,0.16;
+    0.3,0,0.16;
+    0.16,0.16,0];
+
 
 %Initialize velocity vector
 dxi = zeros(2, N);
@@ -70,7 +64,7 @@ dxi = zeros(2, N);
 state = 1;
 
 % These are gains for our formation control algorithm
-formation_control_gain = 10;
+formation_control_gain =20;
 
 
 
@@ -79,13 +73,14 @@ formation_control_gain = 10;
 % Single-integrator -> unicycle dynamics mapping
 si_to_uni_dyn = create_si_to_uni_mapping2('LinearVelocityGain', 0.25, 'AngularVelocityLimit', 7.5);
 % Single-integrator barrier certificates
-si_barrier_cert = create_si_barrier_certificate('SafetyRadius', 0.22);
+si_barrier_cert = create_si_barrier_certificate('SafetyRadius', 0.15);
 % Single-integrator position controller
 si_pos_controller = create_si_position_controller();
 
-waypoints = 0.95*[1.3 0; -1.4 0;]';
+% waypoints = [1 0.5; -1 0.5; -1 -0.5; 1 -0.5]';
+waypoints = [0.7 0.6; -.7 .6; -1.3 0; -.7 -.6; .7 -.6; 1.3 0]';
 close_enough = 0.05;
-
+state = 1;
 for t = 1:iterations
     
     % Retrieve the most recent poses from the Robotarium.  The time delay is
@@ -93,11 +88,8 @@ for t = 1:iterations
     x = r.get_poses();
     
     %% Algorithm
-    
+    waypoint = waypoints(:, state);
     for i = 1:N
-        if (i == 3)
-            %%Nothing here
-        else
         %Zero velocity and get the topological neighbors of agent i
         dxi(:, i) = [0 ; 0];
         
@@ -106,31 +98,61 @@ for t = 1:iterations
         for j = neighbors
             if (norm(x(1:2, 3) - x(1:2, i))^2 < weights(i,3))
                 dxi(:,i) = 0;
+            elseif (i==3)
+                dxi(:, i) = dxi(:, i) + ...
+                    formation_control_gain*...
+                    norm(si_pos_controller(x(1:2, 3), waypoint)/(N-1))*...
+                    (norm(x(1:2, j) - ...
+                    x(1:2, i))^2 - weights(i,j)^2)*(x(1:2, j) - ...
+                    x(1:2, i)) + ...
+                    0.07*formation_control_gain*...
+                    si_pos_controller(x(1:2, 3), waypoint)/(N-1);
             else
-            dxi(:, i) = dxi(:, i) + ...
-                formation_control_gain*(norm(x(1:2, j) - x(1:2, i))^2 -  weights(i,j)^2)*(x(1:2, j) - x(1:2, i));
+                dxi(:, i) = dxi(:, i) + ...
+                    formation_control_gain*(norm(x(1:2, j) - ...
+                    x(1:2, i))^2 - weights(i,j)^2)*(x(1:2, j) - ...
+                    x(1:2, i));
             end
+            
         end
+        if (norm(dxi(:,i))>r.max_linear_velocity)
+            dxi(:,i) = r.max_linear_velocity*(dxi(:,i)./norm(dxi(:,i)));
         end
-    end
-    
-    %% Make the leader travel between waypoints
-    
-    waypoint = waypoints(:, state);
-    
-    switch state        
-        case 1
-            dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
-            if(norm(x(1:2, 3) - waypoint) < close_enough)
-                state = 2;
-            end
-        case 2
-            dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
-            if(norm(x(1:2, 3) - waypoint) < close_enough)
-                state = 1;
-            end
        
     end
+    if(norm(x(1:2, 3) - waypoint) < close_enough)
+        state = state+1;
+        if (state == 7)
+            state=1;
+        end
+    end
+    %% Make the leader travel between waypoints
+    
+%    waypoint = waypoints(:, state);
+%     
+%     switch state        
+%         case 1
+%             dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
+%             if(norm(x(1:2, 3) - waypoint) < close_enough)
+%                 state = 2;
+%             end
+%         case 2
+%             dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
+%             if(norm(x(1:2, 3) - waypoint) < close_enough)
+% %                 state = 3;
+%                   state = 1;
+%             end
+% %         case 3
+% %             dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
+% %             if(norm(x(1:2, 3) - waypoint) < close_enough)
+% %                 state = 4;
+% %             end
+% %         case 4
+% %             dxi(:, 3) = si_pos_controller(x(1:2, 3), waypoint);
+% %             if(norm(x(1:2, 3) - waypoint) < close_enough)
+% %                 state = 1;
+% %             end
+%     end
     
     %% Use barrier certificate and convert to unicycle dynamics
     dxi = si_barrier_cert(dxi, x);
